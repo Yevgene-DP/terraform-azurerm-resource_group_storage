18d64509b26aa757315d53a4290be902be10b50f
Yevgene-DP <ebalalay@gmail.com>
2025-10-06 19:28:34 +0300

    Solution FOR MAIN.TF

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ module "resource_group_storage" {
   storage_account_name = "stterraformex123"
   create_container     = true
   container_name       = "terraform-files"
+# solution all errors
 
   tags = {
     Environment = "Development"
